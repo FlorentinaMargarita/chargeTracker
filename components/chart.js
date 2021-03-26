953ee0d5462bef3e564576bed8fd90ba1a5708d0
FloTheFlower <florentina.hofbauer@gmail.com>
2021-03-25 19:43:38 -0700

    feat: charts thing shows TIME

--- components/chart.js
+++ components/chart.js
@@ -3,13 +3,17 @@ import { LineChart, Grid, YAxis, XAxis } from "react-native-svg-charts";
 import "react-native-svg";
 import { View } from "react-native";
 import { useTheme } from "@react-navigation/native";
+import * as scale from 'd3-scale';
 
 export default function Chart({ data }) {
   const { colors } = useTheme();
   const verticalContentInset = { top: 10, bottom: 10 };
 
-  const axesSvg = { fontSize: 10, fill: "grey" };
-  const xAxisHeight = 30;
+  const axesSvg = { fontSize: 10, fill: "grey", height: 200, };
+  const xAxisHeight = 1;
+
+  const spacingInner = 1
+const spacingOuter = 1
 
   const chartData = [
     {
@@ -22,14 +26,13 @@ export default function Chart({ data }) {
     },
   ];
 
-
   return (
-    <View style={{ height: 200, padding: 20, flexDirection: "row" }}>
+    <View style={{ height: "70%", padding: 20, flexDirection: "row" }}>
       <YAxis
         data={data.actual_energy_delivered.concat(
           data.predictive_energy_delivered
         )}
-        style={{ marginBottom: xAxisHeight }}
+        style={{}}
         contentInset={verticalContentInset}
         svg={axesSvg}
       />
@@ -42,20 +45,25 @@ export default function Chart({ data }) {
         >
           <Grid />
         </LineChart>
+
         <XAxis
-          style={{ marginHorizontal: -10, height: xAxisHeight }}
+          style={{ marginHorizontal: -10, lineHeight: 50, borderWidth: 1, height: 200, backgroundColor: 'red' }}
           data={data.time}
           // xAccessor is a function that takes an index and returns the x value at that index
-          // index is an object. 
-          xAccessor={({item}) => {
-            console.log(index, "index", data.time[index], "data.time[index]")
-            return item}}
+          // index is an object.
+          xAccessor={({ item }) => {
+            return item;
+          }}
           formatLabel={(value) => {
-          console.log(value, "value"); 
-          console.log(data.time, "data.time")
-          return new Date(value * 1000).toLocaleDateString()}}
+            console.log(value, "value");
+            console.log(data.time, "data.time");
+            return new Date(value * 1000).toLocaleString();
+          }}
+          // scale={scale.scaleBand}
+          spacingInner={spacingInner}
+          spacingOuter={spacingOuter}
           contentInset={{ left: 10, right: 10 }}
-          svg={{...axesSvg, rotation: 30, y:10}}
+          svg={{ ...axesSvg, lineHeight: 500, rotation: 30, y: 50, viewbox:"0 0 46 46" }}
         />
       </View>
     </View>
