23287f8d7b88a83a8add5246aac1ee5d90db4ae4
FloTheFlower <florentina.hofbauer@gmail.com>
2021-03-26 19:01:19 -0700

    .

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ export default function App() {
     <NavigationContainer theme={MyTheme}>
       <Stack.Navigator>
         <Stack.Screen
-          screenOptions={{
+          options={{
             headerShown: false,
           }}
           name="LandingScreen"
